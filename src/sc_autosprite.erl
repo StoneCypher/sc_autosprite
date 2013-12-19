b039ce36074495cd2c89e58787ee6731a14dc7cf
John Haugeland <stonecypher@gmail.com>
2013-12-18 22:24:22 -0800

    src/sc_autosprite.erl

---
+++ src/sc_autosprite.erl
@@ -0,0 +1,68 @@
+
+-module(sc_autosprite).
+
+
+
+
+
+-export([
+
+    layout/1
+
+]).
+
+
+
+
+
+layout(Sizes) ->
+
+    MasterLength = length(Sizes),
+    CheckLength  = length([ 1 || {_Name,X,Y} <- Sizes, is_integer(X), is_integer(Y), X > 0, Y > 0 ]),
+
+    if MasterLength =/= CheckLength -> throw({ error, "All layout sizes must be {X,Y} positive integer pixel sizes" }); true -> ok end,
+
+    Areas                      = [ X*Y || {_Name,X,Y} <- Sizes ],
+    SumArea                    = lists:sum(Areas),
+    
+    { Names, Widths, Heights } = lists:unzip3(Sizes),
+    { MinWidth, MaxWidth }     = sc:extrema(Widths),
+    { MinHeight, MaxHeight }   = sc:extrema(Heights),
+
+    WP = width_pack(MaxWidth, Sizes),
+
+    { WP, Areas, SumArea, MinWidth, MaxWidth, MinHeight, MaxHeight }.
+
+
+
+
+
+width_pack(MaxWidth, Sizes) ->
+
+    WAreas = lists:keysort(2, Sizes),
+
+    [seq_pack(MaxWidth, WAreas), MaxWidth].
+
+
+
+
+
+seq_pack(Width, Sizes) ->
+
+    seq_pack(Width, Sizes, [], 0).
+
+
+
+
+
+seq_pack(_Width, [], Layout, _AtHeight) ->
+
+    Layout;
+
+
+
+
+
+seq_pack(Width, [ThisSize|RemSizes], Layout, AtHeight) ->
+
+    seq_pack(Width, RemSizes, [ThisSize]++Layout).
